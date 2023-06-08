616c06288191a4f299b55dff7ba42d16f8c883f7
Jan Bogosyan <janbogosyan@gmail.com>
2023-06-08 20:43:46 +0300

    all files

---
+++ src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -0,0 +1,21 @@
+import './CourseGoalItem.css';
+import React from 'react';
+
+
+
+
+const CourseGoalItem = props => {
+  
+  const deleteHandler = () => {
+    props.onDelete(props.id);              
+  };
+
+  return (
+    
+    <li className="goal-item" onClick={deleteHandler}>            
+      {props.children}
+    </li>
+  );
+};
+
+export default CourseGoalItem;
